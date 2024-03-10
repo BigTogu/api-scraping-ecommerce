8e7f709c52bee09ad231d36b308a8039b203d67e
bigTogu <silvia.kenaan@gmail.com>
2024-03-10 13:55:16 +0100

    feat: add console log

--- src/services/puppeteerService.js
+++ src/services/puppeteerService.js
@@ -2,6 +2,7 @@ import puppeteer from "puppeteer";
 
 const launchBrowser = async () => {
   try {
+    console.log("Lanzando el navegador...");
     return await puppeteer.launch({ headless: true, args: ["--no-sandbox"] });
   } catch (error) {
     console.error("Error al lanzar el navegador:", error);

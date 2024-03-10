8e50b8245451c57ea1f97d702e8494bfb25cc0a3
bigTogu <silvia.kenaan@gmail.com>
2024-03-10 18:27:14 +0100

    a

--- src/services/puppeteerService.js
+++ src/services/puppeteerService.js
@@ -11,7 +11,7 @@ const launchBrowser = async () => {
   try {
     console.log("Lanzando el navegador...");
     return await puppeteer.launch({
-      headless: process.env.NODE_ENV === "development" ? false : true,
+      headless: true,
       product: "chrome",
       defaultViewport: null,
       timeout: 17000,
@@ -51,31 +51,24 @@ export const getPriceFromUrl = async (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
       "Accept-Language": "en-US,en;q=0.9",
     });
-    await page.setJavaScriptEnabled(true);
     await page.goto(productUrl);
+    await page.setJavaScriptEnabled(true);
+
     let content = await page.content();
     console.log(content, "--------content");
     try {
       try {
-        content = await page.content();
-        console.log(content, "--------starting content");
         await page.screenshot({ path: "locking.png" });
-        await delay(7000);
-        console.log("waited 7 seconds...");
-        content = await page.content();
-        console.log(content, "--------content after waiting");
         await page.waitForSelector(classNameDenyBtn, {
           visible: true,
           timeout: 7000,
         });
         content = await page.content();
-        console.log(content, "--------before click content");
         await page.screenshot({ path: "before-click.png" });
         const form = await page.$(classNameDenyBtn);
         await form.evaluate((form) => form.click());
         await page.screenshot({ path: "after-click.png" });
         content = await page.content();
-        console.log(content, "--------after click content");
       } catch (error) {
         await page.screenshot({ path: "catch_error.png" });
         console.error("Error al hacer click en el botón de cookies:", error);
@@ -92,7 +85,6 @@ export const getPriceFromUrl = async (
       const price = await page.$eval(classNamePrice, (element) =>
         element ? element.innerText : null
       );
-      console.log(price, "--------price");
 
       await closeBrowser(browser);
 

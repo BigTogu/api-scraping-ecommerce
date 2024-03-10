130b2a222a2ec3c150afa5495bacd87075d965dc
bigTogu <silvia.kenaan@gmail.com>
2024-03-10 16:18:40 +0100

    a

--- src/services/puppeteerService.js
+++ src/services/puppeteerService.js
@@ -40,40 +40,45 @@ export const getPriceFromUrl = async (
     });
 
     await page.goto(productUrl);
-
     try {
-      await page.screenshot({ path: "before-click.png" });
-      await page.waitForSelector(classNameDenyBtn, {
+      try {
+        await page.screenshot({ path: "locking.png" });
+        await page.waitForSelector(classNameDenyBtn, {
+          visible: true,
+          timeout: 7000,
+        });
+        await page.screenshot({ path: "before-click.png" });
+        const form = await page.$(classNameDenyBtn);
+        await form.evaluate((form) => form.click());
+        await page.screenshot({ path: "after-click.png" });
+      } catch (error) {
+        await page.screenshot({ path: "catch_error.png" });
+        console.error("Error al hacer click en el botón de cookies:", error);
+      }
+
+      // Esperar a que el selector de precio esté presente en la página
+      await page.waitForSelector(classNamePriceSelector, {
         visible: true,
-        timeout: 7000,
+        timeout: 100,
       });
-      const form = await page.$(classNameDenyBtn);
-      await form.evaluate((form) => form.click());
-      await page.screenshot({ path: "after-click.png" });
+      await page.screenshot({ path: "info.png" });
+
+      // Obtener el precio utilizando el selector proporcionado
+      const price = await page.$eval(classNamePrice, (element) =>
+        element ? element.innerText : null
+      );
+      console.log(price, "--------price");
+
+      await closeBrowser(browser);
+
+      return price;
     } catch (error) {
-      await page.screenshot({ path: "catch_error.png" });
-      console.error("Error al hacer click en el botón de cookies:", error);
+      console.error("Global: Error al obtener el precio del producto:", error);
+      await closeBrowser(browser);
+      return null;
     }
-
-    // Esperar a que el selector de precio esté presente en la página
-    await page.waitForSelector(classNamePriceSelector, {
-      visible: true,
-      timeout: 100,
-    });
-    await page.screenshot({ path: "info.png" });
-
-    // Obtener el precio utilizando el selector proporcionado
-    const price = await page.$eval(classNamePrice, (element) =>
-      element ? element.innerText : null
-    );
-    console.log(price, "--------price");
-
-    await closeBrowser(browser);
-
-    return price;
   } catch (err) {
     console.error(err);
-    await page.screenshot({ path: "global_catch.png" });
     await closeBrowser(browser);
   }
 };
